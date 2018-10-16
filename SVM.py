c720d5b218fa0cd8283b3c70078e57d48741f1bb
Siriuslight <subhampal198@gmail.com>
2018-10-16 06:18:18 +0530

    Added more files

---
+++ SVM.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+#Hard code simple data set
+#x = [1, 5, 1.5, 8, 1, 9]
+#y = [2, 8, 1.8, 8, 0.6, 11]
+#plt.scatter(x,y)
+#plt.show()
+X = np.array([[1,2],[5,8],[1.5,1.8],[8,8],[1,0.6],[9,11]])
+y = [0,1,0,1,0,1]
+clf = svm.SVC(kernel='linear', C = 1.0)
+clf.fit(X,y)
+w = clf.coef_[0]
+print(w)
+
+a = -w[0] / w[1]
+xx = np.linspace(0,12)
+yy = a * xx - clf.intercept_[0] / w[1]
+plt.plot(xx, yy, 'k-', label="Non weighted div")
+plt.scatter(X[:, 0], X[:, 1], c = y)
+plt.legend()
+plt.show()
+
+print("Prediction of target for 0.58,0.76 values:")
+t= clf.predict([[0.58,0.76]])
+print(t)
+print("Prediction of target for given value")
+t= clf.predict([[10.58,10.76]])
+print(t)
